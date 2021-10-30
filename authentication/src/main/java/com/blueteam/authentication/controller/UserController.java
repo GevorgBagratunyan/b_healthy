18f5cd6876726cb863725d62bc6bcf916d30f9f0
mouradianelen <elen_muradyan@edu.aua.am>
2021-10-30 16:41:33 +0400

    initial commit for security service

---
+++ authentication/src/main/java/com/blueteam/authentication/controller/UserController.java
@@ -0,0 +1,74 @@
+package com.blueteam.authentication.controller;
+
+
+import com.blueteam.authentication.entity.Role;
+import com.blueteam.authentication.entity.User;
+import com.blueteam.authentication.service.UserService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(@Lazy UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/users/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("user/users/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+
+    }
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("user/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+
+    }
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?>  addRoleToUser(@RequestBody RoleToUserForm form){
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+    @Data
+    class RoleToUserForm{
+        private String username;
+        private String roleName;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getRoleName() {
+            return roleName;
+        }
+
+        public void setRoleName(String roleName) {
+            this.roleName = roleName;
+        }
+    }
+
+
+}
---
+++ authentication/src/main/java/com/blueteam/authentication/repo/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.blueteam.authentication.repo;
+
+import com.blueteam.authentication.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Role findByName(String name);
+}
