93f411fe28a6589ca02b571700fb255bccf189ad
GevorgBagratunyan <g.bagratunyan@gmail.com>
2021-10-31 15:16:35 +0400

    Twilio Account SID and Token moved to local file, for not pushing to GitHub and not expose it for public access.

--- notifier/src/main/java/com/blueteam/notifier/service/SmsService.java
+++ notifier/src/main/java/com/blueteam/notifier/service/SmsService.java
@@ -8,15 +8,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SmsService {
-    private static final String ACCOUNT_SID = "AC2267645b6d40d4a63f869278a6d2daeb";
-    private static final String AUTH_TOKEN = "63d7afce40c28b8a698cb33ffb8aa181";
-    private static final String TWILIO_NUMBER = "+12055286027";
 
     public void sendSms(String phoneNumber, String smsText) {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        MessageCreator message = Message.create(ACCOUNT_SID,
+        Twilio.init(TwilioConfigs.ACCOUNT_SID, TwilioConfigs.AUTH_TOKEN);
+        MessageCreator message = Message.create(TwilioConfigs.ACCOUNT_SID,
                 new PhoneNumber(phoneNumber),
-                new PhoneNumber(TWILIO_NUMBER),
+                new PhoneNumber(TwilioConfigs.TWILIO_NUMBER),
                 smsText);
         message.execute();
     }
